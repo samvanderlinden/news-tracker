ba01d726f8126bfe8a4cb8346ecbde3f296a930d
Sam Vanderlinden <VANDSAM@chrobinson.com>
2021-07-24 15:47:28 -0500

    add Article model, cross env library

--- checklist.md
+++ checklist.md
@@ -1,5 +1,5 @@
 # Check List
 - [x] Create server side auth, post, and verify token routes
-- [ ] Get news api key
+- [x] Get news api key
 - [ ] Create server GET request for user to get news articles
 - [ ] Finish error handling in server side routes
---
+++ server/model/Article.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const articleSchema = new mongoose.Schema({
+  source: { type: String, required: true },
+  author: { type: String, required: false },
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  content: { type: String, required: true },
+  publishedAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Article', articleSchema);

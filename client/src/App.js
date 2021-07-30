44432391b311e252c1d2d2cda3c8670cc1207bee
Sam Vanderlinden <samvanderlinden6@gmail.com>
2021-07-29 19:24:37 -0500

    add react bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,58 @@
 import { useState } from 'react';
-import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import User from './components/User/User';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const logoutHandler = () => {
     setIsLoggedIn(false)
   }
 
+  // return (
+  //   <div>
+  //     <Header />
+  //     {isLoggedIn ? <User onLogout={logoutHandler} /> : <Login setIsLoggedIn={setIsLoggedIn} />}
+  //   </div>
+  // );
   return (
-    <div>
-      <Header />
-      {isLoggedIn ? <User onLogout={logoutHandler} /> : <Login setIsLoggedIn={setIsLoggedIn} />}
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/user">User</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/user">
+            <User onLogout={logoutHandler} />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;

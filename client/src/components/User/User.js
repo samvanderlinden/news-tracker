d956655aac4320a5c8d1a9eea4741186e491d0f3
Sam Vanderlinden <samvanderlinden6@gmail.com>
2021-07-31 11:32:09 -0500

    styling article cards

--- client/src/components/User/User.js
+++ client/src/components/User/User.js
@@ -1,20 +1,35 @@
 import { dummyData } from "../../dummy-data";
+import { Grid, Row, Col, Card, CardGroup } from 'react-bootstrap';
+import noImage from '../../assets/no-image.jpg'
 
-const User = ({ onLogout, setIsLoggedIn }) => {
-
-  // useEffect(() => {
-  //   setIsLoggedIn(true);
-  // })
+const User = ({ onLogout }) => {
 
   const articles = dummyData.map(article => {
-    return <li key={article.source.id}>{article.title}</li>
+    const image = article.urlToImage ? article.urlToImage : noImage
+    return (
+      <Card key={article.title} style={{ 'overflow': 'auto' }}>
+        <Card.Img variant="top" src={image} />
+        <Card.Body>
+          <Card.Title>{article.title}</Card.Title>
+          <Card.Text>
+            This is a longer card with supporting text below as a natural
+            lead-in to additional content. This content is a little bit longer.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    )
   })
+
 
   return (
     <div>
       <h3>User component</h3>
       <button onClick={onLogout}>Logout</button>
-      <ul>{articles}</ul>
+      <CardGroup>
+        <Row xs={1} sm={2} md={3} >
+          {articles}
+        </Row>
+      </CardGroup>
     </div>
   )
 }
--- client/src/dummy-data.js
+++ client/src/dummy-data.js
@@ -37,5 +37,70 @@ export const dummyData = [
     "urlToImage": null,
     "publishedAt": "2021-07-28T12:18:55+00:00",
     "content": "Skip to main content\r\nThis content is only available to USA TODAY subscribers.\r\nSubscribe for as low as $4.99 per month.\r\nYour subscription includes access to:\r\nExclusive, subscriber-only content and… [+796 chars]"
+  },
+  {
+    "source": {
+      "id": "cbc-news",
+      "name": "CBC News"
+    },
+    "author": "CBC News",
+    "title": "Women are dominating Canada's performance at the Olympics — and we shouldn't be surprised | CBC Sports",
+    "description": "Everywhere you turn at the Tokyo Olympics, there are Canadian women carving a new path for women in sport in their country. Through five days of competition in Japan, Canada has claimed nine medals — all by women.",
+    "url": "http://www.cbc.ca/sports/olympics/summer/canadian-women-dominating-heroux-feature-1.6120359",
+    "urlToImage": "https://i.cbc.ca/1.6120434.1627468786!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_620/1234167350.jpg",
+    "publishedAt": "2021-07-28T11:52:21.2102681Z",
+    "content": "Everywhere you turn at the Tokyo Olympics, there are Canadian women carving a new path for women in sport in their country.\r\nThrough five days of competition in Japan, Canada has won nine medals. Wom… [+5989 chars]"
+  },
+  {
+    "source": {
+      "id": "cbs-news",
+      "name": "CBS News"
+    },
+    "author": "CBS News",
+    "title": "Eye Opener: CDC recommends masks again",
+    "description": "The CDC now says vaccinated Americans should resume wearing masks indoors in certain parts of the country. Also, Simone Biles pulls out of the individual all-around gymnastics competition at the Tokyo Olympics. All that and all that matters in today’s Eye Ope…",
+    "url": "https://www.cbsnews.com/video/eye-opener-cdc-recommends-masks-again/",
+    "urlToImage": "https://cbsnews2.cbsistatic.com/hub/i/r/2021/07/28/74bc6e2d-faf1-493b-85b2-4d5fe2ae35b0/thumbnail/1200x630/1b6dc82c894f20acfd977dc694f27747/0728-ctm-eyeopener7am-761816-640x360.jpg",
+    "publishedAt": "2021-07-28T11:44:08+00:00",
+    "content": "Watch CBSN Live\r\nCopyright © 2021 CBS Interactive Inc. All rights reserved.\r\nGet browser notifications for breaking news, live events, and exclusive reporting.\r\nNot NowTurn On"
+  },
+  {
+    "source": {
+      "id": "abc-news-au",
+      "name": "ABC News (AU)"
+    },
+    "author": "ABC News",
+    "title": "Tokyo Olympics schedule for Thursday: Here's when to watch the swimming, hockey and golf",
+    "description": "There are another five finals in the pool on Thursday, while Australia's women's rugby sevens team begin the defence of their Olympic title. Here are the events to watch out for on day 6.",
+    "url": "http://www.abc.net.au/news/2021-07-28/olympic-games-tokyo-2020-schedule-thursday-swimming-hockey-golf/100331348",
+    "urlToImage": "https://live-production.wcms.abc-cdn.net.au/4e1ed2fe911e7ae6d376afec1f5b0f1f?impolicy=wcms_crop_resize&cropH=1435&cropW=2551&xPos=0&yPos=133&width=862&height=485",
+    "publishedAt": "2021-07-28T11:13:29Z",
+    "content": "There are another five finals in the pool on Thursday, while Australia's women's rugby sevens team begin the defence of their Olympic title.\r\nIt's also the first day of the golf and the BMX racing.\r\n… [+7205 chars]"
+  },
+  {
+    "source": {
+      "id": "the-huffington-post",
+      "name": "The Huffington Post"
+    },
+    "author": "AP",
+    "title": "Tokyo Reports Yet Another All-Time High Of Daily COVID-19 Infections",
+    "description": "Experts say the surge amid the Tokyo Olympics is being propelled by the new, more contagious delta variant of the coronavirus.",
+    "url": "https://www.huffpost.com/entry/covid-19-outbreak-japan-tokyo-olympics_n_610139bde4b0fd216c209ed2",
+    "urlToImage": "https://img.huffingtonpost.com/asset/61013ad13b0000a17fecd548.jpeg?ops=1778_1000",
+    "publishedAt": "2021-07-28T11:10:07Z",
+    "content": "TOKYO (AP) Tokyo reported 3,177 new coronavirus cases on Wednesday, setting an all-time high and exceeding 3,000 for the first time days after the start of the Olympics.\r\nThe new cases exceeded the e… [+3305 chars]"
+  },
+  {
+    "source": {
+      "id": "financial-times",
+      "name": "Financial Times"
+    },
+    "author": null,
+    "title": "Tokyo Olympics Daily: Simone Biles pulls out of second event",
+    "description": "Day 4: Stars open up about mental health, Japan tastes golden glory and stage set for bruising rugby final",
+    "url": "https://www.ft.com/content/a01c72c0-aa0b-47be-84e4-dd21fbdcc573",
+    "urlToImage": "https://www.ft.com/__origami/service/image/v2/images/raw/https%3A%2F%2Fd1e00ek4ebabms.cloudfront.net%2Fproduction%2F1b90a1da-b11d-4463-9246-64278b94472c.jpg?source=next-opengraph&fit=scale-down&width=900",
+    "publishedAt": "2021-07-28T10:52:25.6309625Z",
+    "content": "Simone Biles will not defend her individual all-around title at the Tokyo Olympics, USA Gymnastics said on Wednesday, a day after her shocking withdrawal from the women’s team event underscored the i… [+7112 chars]"
   }
 ]

496dc41fe1ccbf6a323af816bbcab79954946aee
Sam Vanderlinden <samvanderlinden6@gmail.com>
2022-02-24 14:29:23 -0600

    update auth card-header height to 40px

--- client/src/components/AuthCard/AuthCard.js
+++ client/src/components/AuthCard/AuthCard.js
@@ -7,26 +7,25 @@ const AuthCard = (props) => {
   return (
     <div className={classes.container}>
       <Form onSubmit={props.submit} className={classes.card}>
-        <div>
-          <div className={classes["card-header"]}>
-            <h2>{props.header}</h2>
-          </div>
-          <div className={classes.inputs}>
-            {props.children}
+        <div className={classes["card-header"]}>
+          {/* <h2>{props.header}</h2> */}
+          {props.header}
+        </div>
+        <div className={classes.inputs}>
+          {props.children}
 
-            <Button
-              type="submit"
-              variant="primary"
-              className={classes["submit-button"]}
-            >
-              Submit
-            </Button>
-            <br />
-            <Link to={`/${props.mainScreen}`}>
-              {props.mainScreen.charAt(0).toUpperCase() +
-                props.mainScreen.slice(1)}
-            </Link>
-          </div>
+          <Button
+            type="submit"
+            variant="primary"
+            className={classes["submit-button"]}
+          >
+            Submit
+          </Button>
+          <br />
+          <Link to={`/${props.mainScreen}`}>
+            {props.mainScreen.charAt(0).toUpperCase() +
+              props.mainScreen.slice(1)}
+          </Link>
         </div>
       </Form>
     </div>

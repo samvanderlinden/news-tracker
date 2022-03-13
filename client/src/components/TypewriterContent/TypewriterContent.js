0ce7e04e6ef4f43a5a9493d208b9cf920ff64b21
Sam Vanderlinden <samvanderlinden6@gmail.com>
2022-03-13 18:01:03 -0500

    make typewriter effect and font responsive

--- client/src/components/TypewriterContent/TypewriterContent.js
+++ client/src/components/TypewriterContent/TypewriterContent.js
@@ -5,12 +5,7 @@ const TypewriterContent = () => {
   return (
     <div className={classes.container}>
       <p className={classes["typed-out-1"]}>Welcome to the News-Tracker app.</p>
-      <p className={classes["typed-out-2"]}>
-        Login or Register now to start viewing
-      </p>
-      <p className={classes["typed-out-3"]}>
-        and saving your favorite news stories!
-      </p>
+      <p className={classes["typed-out-2"]}>Login or Register now.</p>
     </div>
   );
 };
